0d18ca500b44d9ec7f31b30b89994e96ccc0c4b7
Konstantin Kostov <konstantin@headbright.be>
2018-12-02 10:18:19 +0100

    Add length validator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn validate_serial_length(serial_number: &str) -> bool {
+    serial_number.chars().count() >= 6
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn validates_length_of_four_characters_as_invalid() {
+        let test_serial = "i234";
+        let validation_result = validate_serial_length(test_serial);
+        assert_eq!(false, validation_result);
+    }
+
+    #[test]
+    fn validates_length_of_six_characters_as_valid() {
+        let test_serial = "i23456";
+        let validation_result = validate_serial_length(test_serial);
+        assert_eq!(true, validation_result);
+    }
+
+    #[test]
+    fn validates_length_of_ten_characters_as_valid() {
+        let test_serial = "i234567891";
+        let validation_result = validate_serial_length(test_serial);
+        assert_eq!(true, validation_result);
+    }
+}
